31e02a79895010b28727a691d383c9dd8a733a54
Boonie Attawut <puifai.ba@gmail.com>
2021-01-05 03:33:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,9 +168,9 @@ def player_numbers(team_name)
   jersey_numbers = []
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team_name
-      team_date[:players].each do |player|
-        player.each do |key, value|
-          if key == :number
+      team_date.each do |key, value|
+        if key == :player
+          value.each do |player|
             jersey_numbers << value
           end
         end
