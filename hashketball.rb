8c3a1d9c40bd83caf828db13f51fb8e0901cd94b
Boonie Attawut <puifai.ba@gmail.com>
2021-01-05 03:51:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,13 +165,13 @@ def team_names
 end
 
 def player_numbers(team_name)
-  jersey_number = []
+  output = []
   game_hash.each do |team, team_info|
     if team_info[:team_name] == team_name
       team_info.each do |key, value|
         if key == :players
           value.each do |player|
-          jersey_number.push(player[:number])
+          output.push(player[:number])
           end
         end
       end
