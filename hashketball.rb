66ac44525cf84d08a4adef08139ad0724237f76e
Boonie Attawut <puifai.ba@gmail.com>
2021-01-05 03:56:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -186,7 +186,6 @@ def player_stats(player_name)
       if key == :players
         value.each do |player|
           if player_name == player[:player_name]
-            player.delete(:player_name)
             return player
           end
         end
