21437bf11eda07c754a2b82cc5bdddb5c0c9e159
Boonie Attawut <puifai.ba@gmail.com>
2021-01-05 03:58:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -193,3 +193,17 @@ def player_stats(player_name)
     end
   end
 end
+
+def big_shoe_rebounds
+  big_shoe = 0
+  rebounds = 0
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  return rebounds
+end
